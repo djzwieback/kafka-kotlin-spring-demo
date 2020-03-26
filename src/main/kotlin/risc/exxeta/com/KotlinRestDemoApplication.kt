dd0588117e5a595b5987de6c2c9292fb0be60971
Richard Schlenker <richard.schlenker@exxeta.com>
2020-03-26 20:27:53 +0100

    Initial setup

---
+++ src/main/kotlin/risc/exxeta/com/KotlinRestDemoApplication.kt
@@ -0,0 +1,11 @@
+package risc.exxeta.com
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestDemoApplication>(*args)
+}
